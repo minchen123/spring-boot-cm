e09f39d11c681704a3add17ee43b98ba439c5235
joinliang <304504317@qq.com>
2020-07-14 19:05:46 +0800

    项目初始化提交

---
+++ cm-scaffold/src/main/java/com/cm/scaffold/ScaffoldStartUpApplication.java
@@ -0,0 +1,17 @@
+package com.cm.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description 项目启动类，脚手架项目初步搭建
+ * @Author cm
+ * @Date 2020/7/14 16:48
+ */
+
+@SpringBootApplication
+public class ScaffoldStartUpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffoldStartUpApplication.class,args);
+    }
+}
