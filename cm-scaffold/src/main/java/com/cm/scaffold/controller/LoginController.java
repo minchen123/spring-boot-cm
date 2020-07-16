6a98928f0e9fc0e92a3019abe0d08745b36860f5
joinliang <304504317@qq.com>
2020-07-16 18:45:51 +0800

    INIT

---
+++ cm-scaffold/src/main/java/com/cm/scaffold/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.cm.scaffold.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description todo
+ * @Author cm
+ * @Date 2020/7/16 11:12
+ */
+@RequestMapping("/user")
+public class LoginController {
+
+}
