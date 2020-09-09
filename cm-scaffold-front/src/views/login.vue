32807b096adfab5ee74395b5e48c4e4a14cd0fcc
joinliang <304504317@qq.com>
2020-09-09 15:33:46 +0800

    新增登录页

---
+++ cm-scaffold-front/src/views/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login">
+      登陆信息
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {
+
+    }
+  },
+
+  methods() {
+
+  },
+
+  created() {
+
+  },
+
+  mounted() {
+
+  },
+  
+}
+</script>
+
+<style scoped>
+
+</style>
